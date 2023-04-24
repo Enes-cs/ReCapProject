d392555c032ac7c64a79179f31d19c90d88eb8e4
İbrahim Enes ÖZBAY <i.enes43tr@gmail.com>
2023-04-24 21:32:42 +0300

    DataListRuleDelete

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -41,11 +41,6 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 22 & DateTime.Now.Hour == 23)
-            {
-                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
-            }
-
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarListed);
         }
 
