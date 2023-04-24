ea1e5ece62a43d487c5afdfe83d8c8c5eb57638e
İbrahim Enes ÖZBAY <i.enes43tr@gmail.com>
2023-04-24 21:47:12 +0300

    ValidationTool

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -25,14 +26,8 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            var context = new ValidationContext<Car>(car);
-            CarValidator carvalidator = new CarValidator();
-            var result = carvalidator.Validate(context);
-
-            if (!result.IsValid)
-            {
-                throw new ValidationException(result.Errors);
-            }
+           
+            ValidationTool.Validate(new CarValidator(), car);
 
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
